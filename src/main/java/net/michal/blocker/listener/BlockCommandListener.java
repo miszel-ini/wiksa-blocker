37fa8a167660a3af9b2cf32feeb467f2a5c5e279
miszel.ini <michalkolek.pl@gmail.com>
2023-03-10 07:52:43 +0100

    setup command blocker

--- src/main/java/net/michal/blocker/listener/BlockCommandListener.java
+++ src/main/java/net/michal/blocker/listener/BlockCommandListener.java
@@ -1,6 +1,8 @@
 package net.michal.blocker.listener;
 
 import net.michal.blocker.WiksaBlocker;
+import net.michal.blocker.helper.ChatHelper;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -11,6 +13,18 @@ public class BlockCommandListener implements Listener {
 
     @EventHandler
     public void onCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        String command = event.getMessage();
 
+        if(player.isOp() || player.hasPermission("blocker.commands")) {
+            event.setCancelled(false);
+
+        } else {
+            if(instance.getConfig().getStringList("commands").contains(command)) {
+                event.setCancelled(true);
+
+                ChatHelper.sendChatMessage(player, instance.getConfig().getString("command-blocked"));
+            }
+        }
     }
 }
